74d428d03dd0f61685e4976e3e49d841f3dfbcc0
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2024-02-09 15:25:57 +0800

    created basic navbar
    navbar v1

---
+++ src/layout/NavBar/NavBar.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import './NavBar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='navBarContainer'>
+      <div className='navBarLeft'>
+        <ul>
+          <li>
+            <Link href="/"> Home </Link>
+          </li>
+          <li className='searchField'>
+          <input type="text" placeholder='Search Manhwa...'/>
+          </li>
+        </ul>
+      </div>
+      <div className='navBarRight'>
+        <ul>
+          <li>
+            <Link href="/about"> About </Link>
+          </li>
+          <li>
+            <Link href="/manhwas"> Manhwas </Link>
+          </li>
+          <li>
+            <Link href="/login"> Login </Link>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
